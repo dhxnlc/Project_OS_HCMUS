e85a4cbc22b9181757bbf1a6f5ef05d874d3231a
thanhtrietla <95730098+thanhtrietla@users.noreply.github.com>
2022-10-06 23:22:31 +0700

    Update main.cpp

--- OS_Project_01_FAT_NTFS/OS_Project_01_FAT_NTFS/main.cpp
+++ OS_Project_01_FAT_NTFS/OS_Project_01_FAT_NTFS/main.cpp
@@ -2,11 +2,36 @@
 #include <windows.h>
 #include <stdio.h>
 #include"FAT32Reader.h"
+#include "NTFS.h"
+
 int main(int argc, char** argv)
 {
 
     BootsectorReader test('F');
     test.printDetailInformation();
     test.printBootsector();
+    
+    // NTFS
+    wstring diskName;
+    cout << "Enter Disk's Name: ";
+    wcin >> diskName;
+    diskName = L"\\\\.\\" + diskName + L":";
+    LPCWSTR drive = diskName.c_str();
+
+    BYTE sector[512];
+    int check = ReadSector(drive, 0, sector);
+
+    if (check == 1)
+    {
+        Print_Sector(sector);
+
+        Read_BPB(sector, drive);
+    }
+    else
+    {
+        cout << "Failed to read sector!" << endl;
+        return 0;
+    }
+    
     return 0;
 }
